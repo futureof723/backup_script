a1cc5af657b26ce2debf936493d95a47e03a5343
tyler <futureof723@gmail.com>
2024-09-08 09:22:04 -0400

    Added 2 arguments for the user to input. 1) Source Directory 2) Target Directory.
    Added log for backup output in /var/log/backup_script

--- backupscript.sh
+++ backupscript.sh
@@ -1,11 +1,41 @@
 #!/usr/bin/env bash
 
-if [ $? -ne 2 ]
+# check to make sure user has entered exactly two arguments
+if [ $# -ne 2 ]
 then
-  echo "Usage: backup.sh <source_directory> <target_directory>"
+  echo "Usage: $0 <source_directory> <target_directory>"
   echo "Please try again."
   exit 1
 fi
 
+# Check to see if rsync is available
 
+if ! command -v rsync > /dev/null 2>&1
+then
+  echo "This script requires rsync to be installed."
+  echo "Please user your distribution's package manager to install."
+  exit 2
+fi
 
+# Capture the current date, and store it in the format YY-MM-DD
+current_date=$(date +%Y-%m-%d)
+
+# Backup directory
+backup_dir="$2/$current_date"
+
+# Ensure the backup directory exists if not create it
+if [ ! -d "$backup_dir" ]
+then
+  mkdir -p "$backup_dir"
+fi
+
+# Ensure backup_script log directory exists if not create it
+if [ ! -d /var/log/backup_script ]
+then
+  echo "Creating /var/log/backup_script"
+  mkdir /var/log/backup_script
+  echo "/var/log/backup_script created"
+fi
+
+# Perform the backup using rsync and log the output
+$(which rsync) -avb --backup-dir="$backup_dir" --delete "$1" "$2/current" >> "/var/log/backup_script/backup_$current_date.log"
